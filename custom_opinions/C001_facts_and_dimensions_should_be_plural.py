fb3c2bda11242fb2a12ac5e70c9dae778bf07e60
rloredo <rodrigo.loredo@vinted.com>
2024-08-06 19:23:53 +0200

    add pre-commit, ignore ruff cache

--- custom_opinions/C001_facts_and_dimensions_should_be_plural.py
+++ custom_opinions/C001_facts_and_dimensions_should_be_plural.py
@@ -3,6 +3,7 @@ from dbt_opiner.opinions.base_opinion import BaseOpinion
 from dbt_opiner.linter import LintResult, OpinionSeverity
 from dbt_opiner.file_handlers import SQLFileHandler
 
+
 class C001(BaseOpinion):
     def __init__(self):
         super().__init__(
@@ -15,15 +16,15 @@ class C001(BaseOpinion):
 
     def _eval(self, file: SQLFileHandler) -> LintResult:
         if file.dbt_node.alias:
-          if self.is_fact_or_dim(file.dbt_node.schema):
-              if self.is_plural(file.dbt_node.alias):
-                  return LintResult(
-                      file=file,
-                      opinion_code=self.code,
-                      passed=True,
-                      severity=self.severity,
-                      message="Facts and dimensions are plural.",
-                  )
+            if self.is_fact_or_dim(file.dbt_node.schema):
+                if self.is_plural(file.dbt_node.alias):
+                    return LintResult(
+                        file=file,
+                        opinion_code=self.code,
+                        passed=True,
+                        severity=self.severity,
+                        message="Facts and dimensions are plural.",
+                    )
         return LintResult(
             file=file,
             opinion_code=self.code,
@@ -31,19 +32,19 @@ class C001(BaseOpinion):
             severity=self.severity,
             message=f"Facts and dimensions {self.severity.value} be plural.",
         )
-    
+
     @staticmethod
     def is_plural(word):
-      """
-      For now use a simple check for s at the end of the word.
-      In the future we can use a more sofisiticated check with nltk.
-      """
-      if word.endswith('s'):
-        return True
-      return False
-    
+        """
+        For now use a simple check for s at the end of the word.
+        In the future we can use a more sofisiticated check with nltk.
+        """
+        if word.endswith("s"):
+            return True
+        return False
+
     @staticmethod
     def is_fact_or_dim(schema):
-      if re.match(r'.*(fact|dimension).*', schema):
-           return True
-      return False
+        if re.match(r".*(fact|dimension).*", schema):
+            return True
+        return False
--- tests/test_C001.py
+++ tests/test_C001.py
@@ -1,7 +1,6 @@
 import pytest
 from unittest.mock import create_autospec
 from dbt_opiner.file_handlers import SQLFileHandler
-from dbt_opiner.linter import LintResult, OpinionSeverity
 from dbt_opiner.dbt_artifacts import DbtNode
 from custom_opinions.C001_facts_and_dimensions_should_be_plural import C001
 
@@ -11,21 +10,35 @@ def mock_sqlfilehandler(request):
     schema, alias, resource_type, expected_passed = request.param
     mock_handler = create_autospec(SQLFileHandler)
     mock_handler.file_type = ".sql"
-    mock_handler.dbt_node = DbtNode({
-        'schema': schema,
-        'alias': alias,
-        'resource_type': resource_type
-    })
+    mock_handler.dbt_node = DbtNode(
+        {"schema": schema, "alias": alias, "resource_type": resource_type}
+    )
     return mock_handler, expected_passed
 
 
-@pytest.mark.parametrize('mock_sqlfilehandler, expected_passed', [
-    pytest.param(('dimensions', 'dim_customers', 'model', True), True, id='valid_dimension'),
-    pytest.param(('facts', 'fact_sales', 'model', True), True, id='valid_fact'),
-    pytest.param(('facts', 'fact_sale', 'model', False), False, id='invalid_fact_singular'),
-    pytest.param(('dimensions', 'dim_customer', 'model', False), False, id='invalid_dimension_singular'),
-    pytest.param(('staging', 'stg_customer', 'model', False), False, id='valid_staging_singular'),
-], indirect=['mock_sqlfilehandler'])
+@pytest.mark.parametrize(
+    "mock_sqlfilehandler, expected_passed",
+    [
+        pytest.param(
+            ("dimensions", "dim_customers", "model", True), True, id="valid_dimension"
+        ),
+        pytest.param(("facts", "fact_sales", "model", True), True, id="valid_fact"),
+        pytest.param(
+            ("facts", "fact_sale", "model", False), False, id="invalid_fact_singular"
+        ),
+        pytest.param(
+            ("dimensions", "dim_customer", "model", False),
+            False,
+            id="invalid_dimension_singular",
+        ),
+        pytest.param(
+            ("staging", "stg_customer", "model", False),
+            False,
+            id="valid_staging_singular",
+        ),
+    ],
+    indirect=["mock_sqlfilehandler"],
+)
 def test_C001(mock_sqlfilehandler, expected_passed):
     mock_handler, expected_passed = mock_sqlfilehandler
     opinion = C001()
