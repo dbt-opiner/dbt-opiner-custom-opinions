a54363a2176a07f05ce2a3d334b4d7814e0c33f5
rloredo <rodrigo.loredo@vinted.com>
2024-08-15 12:43:36 +0200

    add extra info

--- custom_opinions/C004_project_must_not_send_anon_stats.py
+++ custom_opinions/C004_project_must_not_send_anon_stats.py
@@ -3,4 +3,7 @@
 Sending anonymous statistics is enabled by default (opt-out).
 Although is a good way to help the dbt team improve the product, for privacy
 reasons we recommend to disable this feature.
+
+This opinion checks if the `send_anonymous_usage_stats` parameter is set to false
+in the `dbt_project.yml` file.
 """
